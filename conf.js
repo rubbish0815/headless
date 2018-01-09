b235609ee668fe63f0ab95420e63876f0ce78a95
rubbish0815 <rubbish0815@gmail.com>
2018-01-09 22:53:09 +0100

    changes

--- conf.js
+++ conf.js
@@ -1,77 +1,5 @@
 /*jslint node: true */
 "use strict";
-exports.clientName = 'KT-Hub-Test';
-exports.minClientVersion = '1.10.0';
-
-//https://console.developers.google.com
-exports.pushApiProjectNumber = 0;
-exports.pushApiKey = '';
-
-
-exports.port = 6611;
-//exports.myUrl = 'wss://mydomain.com/bb';
-exports.bServeAsHub = false;
-exports.bSaveJointJson = false;
-exports.bLight = false;
-
-exports.storage = 'sqlite';
-
-exports.hub = 'klaftoshi.spdns.org/bb-test';
-exports.deviceName = 'KT-Hub-Test';
-exports.permanent_paring_secret = 'kt-hub-test';
-exports.control_addresses = ['0HJBA2MU42AICBCFHPL33H75LWKQ5VCAG'];
-exports.payout_address = 'GPDGZDLVLMORZJDA5SEBW4NWFFDK5BUZ';
-exports.KEYS_FILENAME = 'keys.json';
-
-// where logs are written to (absolute path).  Default is log.txt in app data directory
-//exports.LOG_FILENAME = 'log.txt';
-exports.LOG_LEVEL || 6;
-exports.LOG_LEVEL_FILE || 99;
-
-// this is for runnining RPC service only, see play/rpc_service.js
-exports.rpcInterface = '127.0.0.1';
-exports.rpcPort = '7777';
-
-exports.HEARTBEAT_TIMEOUT = 300*1000;
-
-exports.initial_peers = [
-	'wss://byteball.org/bb-test'
-];
-
-
-/*
-// livenet
-exports.initial_witnesses = [
-	'BVVJ2K7ENPZZ3VYZFWQWK7ISPCATFIW3',
-	'DJMMI5JYA5BWQYSXDPRZJVLW3UGL3GJS',
-	'FOPUBEUPBC6YLIQDLKL6EW775BMV7YOH',
-	'GFK3RDAPQLLNCMQEVGGD2KCPZTLSG3HN',
-	'H5EZTQE7ABFH27AUDTQFMZIALANK6RBG',
-	'I2ADHGP4HL6J37NQAD73J7E5SKFIXJOT',
-	'JEDZYC2HMGDBIDQKG3XSTXUSHMCBK725',
-	'JPQKPRI5FMTQRJF4ZZMYZYDQVRD55OTC',
-	'OYW2XTDKSNKGSEZ27LMGNOPJSYIXHBHC',
-	'S7N5FE42F6ONPNDQLCF64E2MGFYKQR2I',
-	'TKT4UESIKTTRALRRLWS4SENSTJX6ODCW',
-	'UENJPVZ7HVHM6QGVGT6MWOJGGRTUTJXQ'
-];
-*/
-
-// testnet
-exports.initial_witnesses = [
-	'2FF7PSL7FYXVU5UIQHCVDTTPUOOG75GX',
-	'2GPBEZTAXKWEXMWCTGZALIZDNWS5B3V7',
-	'4H2AMKF6YO2IWJ5MYWJS3N7Y2YU2T4Z5',
-	'DFVODTYGTS3ILVOQ5MFKJIERH6LGKELP',
-	'ERMF7V2RLCPABMX5AMNGUQBAH4CD5TK4',
-	'F4KHJUCLJKY4JV7M5F754LAJX4EB7M4N',
-	'IOF6PTBDTLSTBS5NWHUSD7I2NHK3BQ2T',
-	'O4K4QILG6VPGTYLRAI2RGYRFJZ7N2Q2O',
-	'OPNUXBRSSQQGHKQNEPD2GLWQYEUY5XLD',
-	'PA4QK46276MJJD5DBOLIBMYKNNXMUVDP',
-	'RJDYXC4YQ4AZKFYTJVCR5GQJF5J6KPRI',
-	'WELOXP3EOA75JWNO6S5ZJHOO3EYFKPIR'
-];
 
 //exports.port = 6611;
 //exports.myUrl = 'wss://mydomain.com/bb';
@@ -82,11 +10,11 @@ exports.bLight = false;
 exports.storage = 'sqlite';
 
 
-exports.hub = 'byteball.org/bb';
-exports.deviceName = 'Headless';
+exports.hub = 'byteball.org/bb-test';
+exports.deviceName = 'KT_Headless';
 exports.permanent_pairing_secret = 'randomstring';
-exports.control_addresses = ['DEVICE ALLOWED TO CHAT'];
-exports.payout_address = 'WHERE THE MONEY CAN BE SENT TO';
+exports.control_addresses = ['0HJBA2MU42AICBCFHPL33H75LWKQ5VCAG'];
+exports.payout_address = 'GPDGZDLVLMORZJDA5SEBW4NWFFDK5BUZ';
 exports.KEYS_FILENAME = 'keys.json';
 
 // where logs are written to (absolute path).  Default is log.txt in app data directory
